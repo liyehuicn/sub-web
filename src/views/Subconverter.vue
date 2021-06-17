f90bf515e3ea1828640002d4439aa89f126d9a4e
liyehui <liyehui@qq.com>
2021-06-17 16:55:39 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -256,7 +256,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "suc.shuiniu.cf (自动编译最新版本后端-稳定）": "https://suc.shuiniu.cf/sub?",
+          "subconverter-6xppwwbzf-liyehuicn.vercel.app (自动编译最新版本后端-稳定）": "https://subconverter-6xppwwbzf-liyehuicn.vercel.app/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
@@ -264,7 +264,7 @@ export default {
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
-          { value: "https://suc.shuiniu.cf/sub?" },
+          { value: "https://subconverter-6xppwwbzf-liyehuicn.vercel.app/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://api.dler.io/sub?" },
           { value: "https://api.wcc.best/sub?" },
